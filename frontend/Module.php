71678a67c3f2685ebbd7b426dfafb3832cb355d6
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-07 09:41:56 +0200

    Init.

---
+++ frontend/Module.php
@@ -0,0 +1,16 @@
+<?php
+namespace bl\cms\nova-poshta\frontend;
+
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'bl\cms\nova-poshta\frontend\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
